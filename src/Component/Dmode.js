5c06db310c063b11e4fecc7488862500228a915f
Agyeiwaa00 <scoogwinkworth@gmail.com>
2024-03-29 18:59:43 +0100

    first commit

---
+++ src/Component/Dmode.js
@@ -0,0 +1,17 @@
+import {useState,useEffect} from "react"
+
+ function DarkMode() {
+
+    const [darkMode, setDarkMode]=useState('white')
+    const click = darkMode=> {setDarkMode(darkMode)}
+    useEffect(()=>{document.body.style.backgroundColor=darkMode},[darkMode])
+    return (
+        <div ClassName={`page ${darkMode && 'dark-mode'}`}>
+            <div className='text-center pt-80' >
+            <button className='dark-mode-button font-bold bg-gray-900 text-white rounded-full m-5 py-1 px-2 w-17' onClick={()=>click('black')}>Dark Mode</button>
+            <button className='light-mode-button font-bold'onClick={()=>click('white')}>Light mode</button>
+            </div>  
+        </div>
+    )
+}
+export default DarkMode;
