d8e310785f63cf802b58d63bcc6521616645b743
Agyeiwaa00 <scoogwinkworth@gmail.com>
2024-04-02 21:27:08 +0200

    update: dark-mode-toggle

--- src/Component/Dmode.js
+++ src/Component/Dmode.js
@@ -8,8 +8,8 @@ import {useState,useEffect} from "react"
     return (
         <div ClassName={`page ${darkMode && 'dark-mode'}`}>
             <div className='text-center pt-80' >
-            <button className='dark-mode-button font-bold bg-gray-900 text-white rounded-full m-5 py-1 px-2 w-17' onClick={()=>click('black')}>Dark Mode</button>
-            <button className='light-mode-button font-bold'onClick={()=>click('white')}>Light mode</button>
+            <button className={`dark-mode-button font-bold bg-gray-900 text-white rounded-full m-5 py-1 px-2 w-17`} onClick={()=>click('black')}>Dark Mode</button>
+            <button className={`light-mode-button font-bold bg-white text-black rounded-full capitalize  m-5 py-1 px-2 w-17 ${darkMode === "black" ? `text-white`:`text-black`}`} onClick={()=>click('white')}>Light mode</button>
             </div>  
         </div>
     )
